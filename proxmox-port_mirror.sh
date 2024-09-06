5c510786c5ff834898195176f230b98980f6dd12
Logan <97614921+loganflecke@users.noreply.github.com>
2024-09-06 15:28:46 -0400

    Clean up syntax and semantics on proxmox-port_mirror.sh

--- proxmox-port_mirror.sh
+++ proxmox-port_mirror.sh
@@ -1,55 +1,56 @@
 #!/bin/bash
 
 # proxmox-port_mirror.sh
-# mirror all traffic on vmbr1 to the interface used by Security Onion for monitoring, tap107i1
+# mirror all traffic on given bridge ports to the interface used by Security Onion for monitoring, tap107i1
 # output all data to a log file
+
+# inspiration comes from "vext" at https://vext.info/2018/09/03/cheat-sheet-port-mirroring-ids-data-into-a-proxmox-vm.html
 
 PORTMIRRORLOG=/root/proxmox-port_mirror.log
 
-ports_mirroring=("vmbr1" "vmbr2")
+mirror_ports=("vmbr1" "vmbr2")
 output_interface="tap107i1" # security onion monitor interface
 
 date > $PORTMIRRORLOG
 echo "#######################" >> $PORTMIRRORLOG
-echo "Mirror port set to ${ports_mirroring[@]}" >> $PORTMIRRORLOG
+echo "Mirror port set to ${mirror_ports[@]}" >> $PORTMIRRORLOG
 
-# Uncomment the following to confirm ports and interfaces as the command is run
-
-#function get_input {
-#	while true; do
-#	read -p "$1" yn >> $PORTMIRRORLOG
-#	case $yn in
-#		[yY] ) echo Proceeding...;
-#			break;;
-#		[nN] ) echo Exiting...;
-#			exit;;
-#		* ) echo Invalid response;;
-#	esac
-#	done
-#}
-#
-#message="Mirror port set to ${ports_mirroring[@]}. Continue? [Y/n]"
-#get_input "$message"
-#message="Output interface set to $output_interface. Continue? [Y/n]"
-#get_input "$message"
+# Uncomment the following to ask for confirmation on what ports to mirror and what interface to output to
+#############################################
+# function get_input {
+# 	while true; do
+# 	read -p "$1" yn >> $PORTMIRRORLOG
+# 	case $yn in
+# 		[yY] ) echo Proceeding...;
+# 			break;;
+# 		[nN] ) echo Exiting...;
+# 			exit;;
+# 		* ) echo Invalid response;;
+# 	esac
+# 	done
+# }
+# get_input "Mirror port set to ${mirror_ports[@]}. Continue? [Y/n]"
+# get_input "Output interface set to $output_interface. Continue? [Y/n]"
+#############################################
 
 echo "Clearing any existing mirror..." >> $PORTMIRRORLOG
 
-for vm_bridge in "${!ports_mirroring[@]}"; do
-	ovs-vsctl clear bridge $ports_mirroring mirrors
+for i in "${!mirror_ports[@]}"; do
+	ovs-vsctl clear bridge $mirror_ports mirrors
 done
 
-for vm_bridge in "${!ports_mirroring[@]}"; do
-	mirror_name="span$((vm_bridge+1))"
-	echo "Creating $mirror_name mirror for ${ports_mirroring[$vm_bridge]} bridge to $output_interface..." >> $PORTMIRRORLOG
+# Create mirror on each bridge listed in mirror_ports
+for i in "${!mirror_ports[@]}"; do
+	mirror_name="span$((i+1))"
+	echo "Creating $mirror_name mirror for ${mirror_ports[$i]} bridge to $output_interface..." >> $PORTMIRRORLOG
 
 	ovs-vsctl -- --id=@p get port $output_interface \
 	-- --id=@m create mirror name=$mirror_name select-all=true output-port=@p \
-	-- set bridge ${ports_mirroring[$vm_bridge]} mirrors=@m >> $PORTMIRRORLOG
+	-- set bridge ${mirror_ports[$i]} mirrors=@m >> $PORTMIRRORLOG
 done
 
 echo "Showing existing mirrors..." >> $PORTMIRRORLOG
 
-ovs-vsctl list Mirror >> $PORTMIRRORLOG
+ovs-vsctl list mirror >> $PORTMIRRORLOG
 
 echo "########################" >> $PORTMIRRORLOG
