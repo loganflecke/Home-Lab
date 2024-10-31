6f6a5360bc2bd82b7ba133a394c6ab1546c9de40
Logan <97614921+loganflecke@users.noreply.github.com>
2024-10-31 16:29:25 -0400

    Update windows-endpoint-enrollment.ps1

--- windows-endpoint-enrollment.ps1
+++ windows-endpoint-enrollment.ps1
@@ -2,4 +2,4 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.10.4-windows-x86_64.zip -OutFile elastic-agent-8.10.4-windows-x86_64.zip
 Expand-Archive .\elastic-agent-8.10.4-windows-x86_64.zip -DestinationPath .
 cd elastic-agent-8.10.4-windows-x86_64
-.\elastic-agent.exe install --url=https://172.18.1.10:8220 --enrollment-token=azQ5WHU1SUJLY2FhZ0RYTzFXU2c6bzBYc3h0Y0RTR3U1ckxKdXdmUENaZw==
+.\elastic-agent.exe install --url=https://172.18.1.10:8220 --enrollment-token=OG5BaDVKSUJ3a2pSSmlHRGUwekM6Rjl6dHgwTWlRUmk3cExrR2pRZWg2UQ==
