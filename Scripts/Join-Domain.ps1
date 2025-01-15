bcc0ec4fae6ae0f892d294bcdab67715085cfb25
Logan <97614921+loganflecke@users.noreply.github.com>
2025-01-15 09:18:49 -0500

    Dynamically get interface name and fix Add-Computer instruction

--- Scripts/Join-Domain.ps1
+++ Scripts/Join-Domain.ps1
@@ -1,10 +1,8 @@
 # Script to Join a computer to a Domain, used before launching an attack from the Attacker machine
 # Written by Logan Flecke
 
-$interface_name = "Ethernet"
 $dns_server = "192.168.1.10"
 $domain_name = "logan.local"
-$server_name = "DC01"
 
 # Enable RDP
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
@@ -12,6 +10,7 @@ Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Write-Host "RDP Enabled"
 
 # Configure IP Addressing
+$interface_name = (Get-NetAdapter).Name
 Write-Host "Setting DNS IP to $dns_server on interface $interface_name"
 
 $interface = Get-NetIPConfiguration -InterfaceAlias $interface_name
@@ -22,4 +21,4 @@ Write-Host "Networking Configured"
 
 # Join to Domain
 Write-Host "Joining to Domain $domain_name"
-Add-Computer -DomainName "LOGAN" -Server "LOGAN\$server_name" -Restart
+Add-Computer -DomainName $domain_name -Credential "Administrator" -Restart
